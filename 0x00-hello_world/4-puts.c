a49a0475d2cdf59b6b0526d6bfb3c509ac0eb5b5
CharityZawadi <zawadicharity8@gmail.com>
2023-09-07 07:09:18 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,16 +3,13 @@
 /**
  * main - Entry point
  *
- * Description: Prints "Programming is like building a multilingual puzzle,"
- *              followed by a new line.
- *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
+int main(void){
 puts("Programming is like building a multilingual puzzle,");
 return (0);
 }
+
 
 
 
