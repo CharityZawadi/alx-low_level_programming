5ad21e2ae5e44b8db18e07af1c5afa4bf37911e4
CharityZawadi <zawadicharity8@gmail.com>
2023-09-12 14:10:04 -0700

    work

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+long int fib1 = 1, fib2 = 2, next;
+int count;
+
+printf("%ld, %ld", fib1, fib2);
+
+for (count = 2; count < 50; count++)
+{
+next = fib1 + fib2;
+printf(", %ld", next);
+fib1 = fib2;
+fib2 = next;
+}
+
+printf("\n");
+
+return (0);
+}
+
