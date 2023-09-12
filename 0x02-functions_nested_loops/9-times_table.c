48c130ab23e7a3c543f74d64a34ae8a74573b3bf
CharityZawadi <zawadicharity8@gmail.com>
2023-09-12 13:23:13 -0700

    ./9-times_table | cat -e

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table, starting with 0.
+ */
+void times_table(void)
+{
+int row, col, product;
+
+for (row = 0; row <= 9; row++)
+{
+for (col = 0; col <= 9; col++)
+{
+product = row * col;
+
+if (col != 0)
+{
+_putchar(',');
+_putchar(' ');
+}
+
+if (product < 10 && col != 0)
+_putchar(' ');
+
+if (product >= 10)
+_putchar((product / 10) + '0');
+
+_putchar((product % 10) + '0');
+}
+_putchar('\n');
+}
+}
+
