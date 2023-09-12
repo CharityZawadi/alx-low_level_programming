22b2a5c59cdba35ed815394eb6bec38f712212e7
CharityZawadi <zawadicharity8@gmail.com>
2023-09-12 11:41:36 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase followed by a new line
+ */
+void print_alphabet(void)
+{
+char letter = 'a';
+
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+
+_putchar('\n');
+}
+
