6dd3c9ce121543a47443db44e2174ea52e6a3991
CharityZawadi <zawadicharity8@gmail.com>
2023-09-13 12:12:58 -0700

    work

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if an integer is positive or negative
+* main - tests the function that prints if an integer is positive or negative
 * Return: 0
 */
 
