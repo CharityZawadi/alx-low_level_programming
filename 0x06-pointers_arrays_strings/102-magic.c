7f8be34e5ba97c4664bac3d87681ac234fac65b0
CharityZawadi <zawadicharity8@gmail.com>
2023-09-21 06:33:41 -0700

    update

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program demonstrates how to print 98 by manipulating
+ *              the integer array a and pointer p.
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 int n;
@@ -9,14 +18,15 @@ int *p;
 a[2] = 1024;
 p = &n;
 /*
-* Add one line of code here to make the program
-* print "a[2] = 98\n" without modifying p or using a.
+* write your line of code here...
+* Remember:
+* - you are not allowed to use a
+* - you are not allowed to modify p
+* - only one statement
+* - you are not allowed to code anything else than this line of code
 */
-
 *(p + 5) = 98;
-
-/* This prints "a[2] = 98\n" */
+/* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
-
