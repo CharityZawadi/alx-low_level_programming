de1a975ac85510ce60f13405abdafc330048ff10
CharityZawadi <zawadicharity8@gmail.com>
2023-09-21 05:22:19 -0700

    updates

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,7 +8,16 @@ int *p;
 
 a[2] = 1024;
 p = &n;
-
+/*
+* write your line of code here...
+* Remember:
+* - you are not allowed to use a
+* - you are not allowed to modify p
+* - only one statement
+* - you are not allowed to code anything else than this line of code
+*/
+;
+/* ...so that this prints 98\n */
 *(p + 5) = 98;
 
 
