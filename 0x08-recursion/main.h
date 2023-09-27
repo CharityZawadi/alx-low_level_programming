58b038f7f324c29b5f8d7a906f37c8240ed4569c
CharityZawadi <zawadicharity8@gmail.com>
2023-09-27 04:27:39 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int sqrt_helper(int n, int guess);
+int is_prime_number(int n);
+int is_prime_helper(int n, int divisor);
+int is_palindrome(char *s);
+int check_palindrome(char *s, int len);
+int wildcmp(char *s1, char *s2);
+
+#endif /* MAIN_H */
+
