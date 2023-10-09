a6220ac3a4d5f2fd5b752af8660a79ea130a679e
CharityZawadi <zawadicharity8@gmail.com>
2023-10-09 04:36:26 -0700

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+#include "0-object_like_macro.h"
+#include "1-pi.h"
+#include "3-function_like_macro.h"
+#include "4-sum.h"
+
+#endif /* MAIN_H */
+
