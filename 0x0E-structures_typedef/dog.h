49bef54678156146122e3b9a557394bea52b0827
CharityZawadi <zawadicharity8@gmail.com>
2023-10-09 05:19:29 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Define a new type struct dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * Description: A structure that represents a dog.
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif /* DOG_H */
+
---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int _putchar(char c);
+
+#include "dog.h"
+#include "dog.h"
+#include "dog.h"
+#include "dog.h"
+#include "dog.h"
+
+#endif /* MAIN_H */
+
