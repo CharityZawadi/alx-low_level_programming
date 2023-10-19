b1149e64180b1b2b5286bc919136b5db0c06c433
CharityZawadi <zawadicharity8@gmail.com>
2023-10-19 14:57:54 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,15 @@
 section .data
-hello db "Hello, Holberton",10 ; The string to print, followed by a newline
-hello_len equ $ - hello
+hello db "Hello, Holberton",0
 
 section .text
 global main
 
 main:
 ; Write the string to stdout (file descriptor 1)
-mov rax, 4           ; syscall number for sys_write
+mov rax, 1           ; syscall number for sys_write
 mov rdi, 1           ; file descriptor 1 (stdout)
 mov rsi, hello       ; pointer to the string
-mov rdx, hello_len   ; length of the string
+mov rdx, 16          ; length of the string
 syscall
 
 ; Exit the program
