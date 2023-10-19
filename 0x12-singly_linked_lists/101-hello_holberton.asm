4d50a170b86ba970a60c66375adc8d3d842e2efa
CharityZawadi <zawadicharity8@gmail.com>
2023-10-19 15:16:20 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,19 @@
 section .data
-hello db "Hello, Holberton",10
+hello db "Hello, Holberton", 10  ; The string with a newline (ASCII value 10)
 
 section .text
 global main
 
 main:
 ; Write the string to stdout (file descriptor 1)
-mov eax, 4           ; syscall number for sys_write
-mov ebx, 1           ; file descriptor 1 (stdout)
-mov ecx, hello       ; pointer to the string
-mov edx, 17          ; length of the string, including the newline
-int 0x80             ; interrupt to invoke syscall
+mov rax, 0x1         ; syscall number for sys_write
+mov rdi, 0x1         ; file descriptor 1 (stdout)
+mov rsi, hello       ; pointer to the string
+mov rdx, 17          ; length of the string, including the newline
+syscall
 
 ; Exit the program
-mov eax, 1           ; syscall number for sys_exit
-xor ebx, ebx         ; status = 0
-int 0x80             ; interrupt to invoke syscall
+mov rax, 60          ; syscall number for sys_exit
+xor rdi, rdi         ; status = 0
+syscall
+
