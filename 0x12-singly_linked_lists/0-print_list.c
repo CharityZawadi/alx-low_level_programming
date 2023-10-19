754dec31a3c6c4e4df71cbb21926f90bbe6c211f
CharityZawadi <zawadicharity8@gmail.com>
2023-10-19 15:27:13 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,15 +8,22 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+size_t count = 0;
 
-	while (h != NULL)
-	{
-		printf("[%u] %s\n", h->len, (h->str ? h->str : "(nil)"));
-		h = h->next;
-		count++;
-	}
+while (h)
+{
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%u] %s\n", h->len, h->str);
+}
+h = h->next;
+count++;
+}
 
-	return (count);
+return (count);
 }
 
