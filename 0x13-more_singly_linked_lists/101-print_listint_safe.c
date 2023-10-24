72bfe98a1e1168b1a1e2615efa54124412c21ea4
CharityZawadi <zawadicharity8@gmail.com>
2023-10-24 13:28:50 -0700

    updates

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,36 +1,39 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * print_listint_safe - Prints a linked list (safely) with a loop
- * @head: A pointer to the head of the list.
+ * print_listint_safe - prints a listint_t linked list with a loop
+ * @head: pointer to the head of the linked list
  *
- * Return: The number of nodes in the list.
+ * Return: Number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-const listint_t *tortoise = head, *hare = head;
-size_t count = 0;
+const listint_t *slow, *fast;
+size_t nodes = 0;
 
 if (head == NULL)
-return (0);
+exit(98);
 
-while (hare != NULL && hare->next != NULL)
+slow = head;
+fast = head;
+
+while (fast != NULL && fast->next != NULL)
 {
-printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-count++;
+printf("[%p] %d\n", (void *)slow, slow->n);
+nodes++;
+slow = slow->next;
+fast = fast->next->next;
 
-tortoise = tortoise->next;
-hare = hare->next->next;
-
-if (tortoise == hare)
+if (slow == fast)
 {
-printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
-break;
+printf("-> [%p] %d\n", (void *)slow, slow->n);
+return (nodes);
 }
 }
 
-return (count);
+printf("[%p] %d\n", (void *)slow, slow->n);
+nodes++;
+return (nodes);
 }
 
