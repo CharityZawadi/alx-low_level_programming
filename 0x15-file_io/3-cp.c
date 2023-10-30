ca87037f680f6a9b8aec537485efde6718aaf027
CharityZawadi <zawadicharity8@gmail.com>
2023-10-30 10:10:31 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,7 +19,6 @@ if (ac != 3)
 dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", av[0]);
 return (1);
 }
-
 *filename_from = av[1];
 *filename_to = av[2];
 return (0);
@@ -29,7 +28,7 @@ return (0);
  * copy_file - Copy the content of a file to another file.
  * @filename_from: Source filename
  * @filename_to: Destination filename
- * Return: 0 on success, 97-100 on failure
+ * Return: 0 on success, 1 on failure
  */
 int copy_file(char *filename_from, char *filename_to)
 {
@@ -41,15 +40,14 @@ fd_from = open(filename_from, O_RDONLY);
 if (fd_from == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename_from);
-return (98);
+return (1);
 }
-
 fd_to = open(filename_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 if (fd_to == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename_to);
 close(fd_from);
-return (99);
+return (1);
 }
 
 while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
@@ -60,7 +58,7 @@ if (bytes_written == -1 || bytes_written != bytes_read)
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename_to);
 close(fd_from);
 close(fd_to);
-return (99);
+return (1);
 }
 }
 
@@ -73,17 +71,17 @@ return (0);
  * main - Main function
  * @ac: Argument count
  * @av: Argument vector
- * Return: 0 on success, 97-100 on failure
+ * Return: 0 on success, 1 on failure
  */
 int main(int ac, char **av)
 {
 char *filename_from, *filename_to;
 
 if (check_arguments(ac, av, &filename_from, &filename_to) != 0)
-return (97);
+return (1);
 
 if (copy_file(filename_from, filename_to) != 0)
-return (98);
+return (1);
 
 return (0);
 }
