cc8fab10b8dede6680030d6b332202357e57b3d6
CharityZawadi <zawadicharity8@gmail.com>
2023-10-30 16:32:40 -0700

    updates

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,89 +1,85 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include "main.h"
+
+#define BUFFER_SIZE 1024
 
 /**
- * error_check - Check for errors and handle them.
- * @code: Error code
- * @msg: Error message
+ * open_source_file - Opens a source file for reading.
+ * @filename: The name of the source file.
+ *
+ * Return: File descriptor if successful, -1 on failure.
  */
-void error_check(int code, char *msg)
-{
-if (code == -1)
-{
-dprintf(STDERR_FILENO, "%s", msg);
-exit(98);
-}
-}
+int open_source_file(const char *filename);
 
 /**
- * copy_file - Copy the content of one file to another.
- * @file_from: Source file descriptor
- * @file_to: Destination file descriptor
+ * open_destination_file - Opens a destination file for writing.
+ * @filename: The name of the destination file.
+ *
+ * Return: File descriptor if successful, -1 on failure.
  */
-void copy_file(int file_from, int file_to)
-{
-char buffer[1024];
-ssize_t bytes;
-
-while ((bytes = read(file_from, buffer, 1024)) > 0)
-{
-if (write(file_to, buffer, bytes) != bytes)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to file\n");
-close(file_from);
-close(file_to);
-exit(99);
-}
-}
-
-if (bytes < 0)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file\n");
-close(file_from);
-close(file_to);
-exit(98);
-}
-}
+int open_destination_file(const char *filename);
 
 /**
- * main - Entry point, copy one file to another.
- * @ac: Argument count
- * @av: Argument vector
- * Return: 0 on success, 97-100 on failure
+ * copy_file - Copies the content of one file to another.
+ * @from_fd: The source file descriptor.
+ * @to_fd: The destination file descriptor.
+ *
+ * Return: 0 on success, -1 on failure.
  */
+int copy_file(int from_fd, int to_fd);
 
-int main(int ac, char **av)
-{
-int file_from, file_to;
+/**
+ * close_file - Closes a file descriptor.
+ * @fd: The file descriptor to close.
+ *
+ * Return: 0 on success, -1 on failure.
+ */
+int close_file(int fd);
 
-if (ac != 3)
+/**
+ * main - Entry point for the copy program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array containing the command-line arguments.
+ *
+ * Return: 0 on success, 97, 98, 99, or 100 on failure.
+ */
+int main(int argc, char *argv[])
 {
-dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", av[0]);
-exit(97);
+int from_fd, to_fd;
+
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+return (97);
 }
 
-file_from = open(av[1], O_RDONLY);
-error_check(file_from, "Error: Can't read from file\n");
+from_fd = open_source_file(argv[1]);
+if (from_fd == -1)
+return (98);
 
-file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-error_check(file_to, "Error: Can't write to file\n");
-
-copy_file(file_from, file_to);
-
-if (close(file_from) == -1)
+to_fd = open_destination_file(argv[2]);
+if (to_fd == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-exit(100);
+close_file(from_fd);
+return (99);
 }
 
-if (close(file_to) == -1)
+if (copy_file(from_fd, to_fd) == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-exit(100);
+close_file(from_fd);
+close_file(to_fd);
+return (99);
 }
+
+if (close_file(from_fd) == -1)
+{
+close_file(to_fd);
+return (100);
+}
+
+if (close_file(to_fd) == -1)
+return (100);
 
 return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,7 +7,10 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int main(int ac, char **av);
-
+int open_source_file(const char *filename);
+int open_destination_file(const char *filename);
+int copy_file(int from_fd, int to_fd);
+int close_file(int fd);
 void display_elf_header(Elf64_Ehdr *header);
 
 #endif /* MAIN_H */
