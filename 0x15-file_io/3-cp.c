9b53319fed89d8274339cb5d77f635591d734996
CharityZawadi <zawadicharity8@gmail.com>
2023-10-30 12:34:06 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,87 +1,89 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include "main.h"
 
 /**
- * check_arguments - Check the command-line arguments.
+ * error_check - Check for errors and handle them.
+ * @code: Error code
+ * @msg: Error message
+ */
+void error_check(int code, char *msg)
+{
+if (code == -1)
+{
+dprintf(STDERR_FILENO, "%s", msg);
+exit(98);
+}
+}
+
+/**
+ * copy_file - Copy the content of one file to another.
+ * @file_from: Source file descriptor
+ * @file_to: Destination file descriptor
+ */
+void copy_file(int file_from, int file_to)
+{
+char buffer[1024];
+ssize_t bytes;
+
+while ((bytes = read(file_from, buffer, 1024)) > 0)
+{
+if (write(file_to, buffer, bytes) != bytes)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to file\n");
+close(file_from);
+close(file_to);
+exit(99);
+}
+}
+
+if (bytes < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file\n");
+close(file_from);
+close(file_to);
+exit(98);
+}
+}
+
+/**
+ * main - Entry point, copy one file to another.
  * @ac: Argument count
  * @av: Argument vector
- * @filename_from: Pointer to the source filename
- * @filename_to: Pointer to the destination filename
- * Return: 0 on success, 1 on failure
+ * Return: 0 on success, 97-100 on failure
  */
-int check_arguments(int ac, char **av,
-char **filename_from, char **filename_to)
+
+int main(int ac, char **av)
 {
+int file_from, file_to;
+
 if (ac != 3)
 {
 dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", av[0]);
-return (1);
-}
-*filename_from = av[1];
-*filename_to = av[2];
-return (0);
+exit(97);
 }
 
-/**
- * copy_file - Copy the content of a file to another file.
- * @filename_from: Source filename
- * @filename_to: Destination filename
- * Return: 0 on success, 1 on failure
- */
-int copy_file(char *filename_from, char *filename_to)
+file_from = open(av[1], O_RDONLY);
+error_check(file_from, "Error: Can't read from file\n");
+
+file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+error_check(file_to, "Error: Can't write to file\n");
+
+copy_file(file_from, file_to);
+
+if (close(file_from) == -1)
 {
-int fd_from, fd_to;
-char buffer[1024];
-ssize_t bytes_read, bytes_written;
-
-fd_from = open(filename_from, O_RDONLY);
-if (fd_from == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename_from);
-return (1);
-}
-fd_to = open(filename_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-if (fd_to == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename_to);
-close(fd_from);
-return (1);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+exit(100);
 }
 
-while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
+if (close(file_to) == -1)
 {
-bytes_written = write(fd_to, buffer, bytes_read);
-if (bytes_written == -1 || bytes_written != bytes_read)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename_to);
-close(fd_from);
-close(fd_to);
-return (1);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+exit(100);
 }
-}
-
-close(fd_from);
-close(fd_to);
-return (0);
-}
-
-/**
- * main - Main function
- * @ac: Argument count
- * @av: Argument vector
- * Return: 0 on success, 1 on failure
- */
-int main(int ac, char **av)
-{
-char *filename_from, *filename_to;
-
-if (check_arguments(ac, av, &filename_from, &filename_to) != 0)
-return (1);
-
-if (copy_file(filename_from, filename_to) != 0)
-return (1);
 
 return (0);
 }
